b916b640e96b8fa14945795cedc682712910e1eb
Solli Honorio <shonorio@gmail.com>
2021-06-23 11:32:48 +0100

    flow to create and get user from database

---
+++ wabanex/lib/wabanex/user.ex
@@ -0,0 +1,25 @@
+defmodule Wabanex.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @fields [:email, :password, :name]
+
+  schema "users" do
+    field :email, :string, null: false
+    field :name, :string, null: false
+    field :password, :string, null: false
+
+    timestamps()
+  end
+
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @fields)
+    |> validate_required(@fields)
+    |> validate_length(:password, min: 6)
+    |> validate_length(:password, min: 2)
+    |> validate_format(:email, ~r/@/)
+    |> unique_constraint([:email])
+  end
+end
